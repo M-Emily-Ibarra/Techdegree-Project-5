b8ac16ac5e56acf356d954a9de82f869270237e8
Emily Ibarra <m.emily.ibarra@gmail.com>
2020-05-05 13:23:11 -0700

    Update photos array to include objects

--- js/scripts.js
+++ js/scripts.js
@@ -4,91 +4,93 @@ function showPhoto(photo) {
 }
 
 const photos = [
-	[
-		'images/01-thumb.jpg',
-		'Hay Bales',
-		'images/01.jpg',
-		'I love hay bales. Took this snap on a drive through the countryside past some straw fields.',
-	],
-	[
-		'images/02-thumb.jpg',
-		'Lake',
-		'images/02.jpg',
-		'The lake was so calm today. We had a great view of the snow on the mountains from here.',
-	],
-	[
-		'images/03-thumb.jpg',
-		'Canyon',
-		'images/03.jpg',
-		'I hiked to the top of the mountain and got this picture of the canyon and trees below.',
-	],
-	[
-		'images/04-thumb.jpg',
-		'Iceberg',
-		'images/04.jpg',
-		'It was amazing to see an iceberg up close, it was so cold but didn\'t snow today.',
-	],
-	[
-		'images/05-thumb.jpg',
-		'Desert',
-		'images/05.jpg',
-		'The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons.',
-	],
-	[
-		'images/06-thumb.jpg',
-		'Fall',
-		'images/06.jpg',
-		'Fall is coming, I love when the leaves on the trees start to change color.',
-	],
-	[
-		'images/07-thumb.jpg',
-		'Plantation',
-		'images/07.jpg',
-		'I drove past this plantation yesterday, everything is so green!',
-	],
-	[
-		'images/08-thumb.jpg',
-		'Dunes',
-		'images/08.jpg',
-		'My summer vacation to the Oregon Coast. I love the sandy dunes!',
-	],
-	[
-		'images/09-thumb.jpg',
-		'Countryside Lane',
-		'images/09.jpg',
-		'We enjoyed a quiet stroll down this countryside lane.',
-	],
-	[
-		'images/10-thumb.jpg',
-		'Sunset',
-		'images/10.jpg',
-		'Sunset at the coast! The sky turned a lovely shade of orange.',
-	],
-	[
-		'images/11-thumb.jpg',
-		'Cave',
-		'images/11.jpg',
-		'I did a tour of a cave today and the view of the landscape below was breathtaking.',
-	],
-	[
-		'images/12-thumb.jpg',
-		'Bluebells',
-		'images/12.jpg',
-		'I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in.',
-	],
+	{
+		thumb: 'images/01-thumb.jpg',
+		alt: 'Hay Bales',
+		src: 'images/01.jpg',
+		caption: 'I love hay bales. Took this snap on a drive through the countryside past some straw fields.',
+	},
+	{
+		thumb: 'images/02-thumb.jpg',
+		alt: 'Lake',
+		src: 'images/02.jpg',
+		caption: 'The lake was so calm today. We had a great view of the snow on the mountains from here.',
+	},
+	{
+		thumb: 'images/03-thumb.jpg',
+		alt: 'Canyon',
+		src: 'images/03.jpg',
+		caption: 'I hiked to the top of the mountain and got this picture of the canyon and trees below.',
+	},
+	{
+		thumb: 'images/04-thumb.jpg',
+		alt: 'Iceberg',
+		src: 'images/04.jpg',
+		caption: 'It was amazing to see an iceberg up close, it was so cold but didn\'t snow today.',
+	},
+	{
+		thumb: 'images/05-thumb.jpg',
+		alt: 'Desert',
+		src: 'images/05.jpg',
+		caption: 'The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons.',
+	},
+	{
+		thumb: 'images/06-thumb.jpg',
+		alt: 'Fall',
+		src: 'images/06.jpg',
+		caption: 'Fall is coming, I love when the leaves on the trees start to change color.',
+	},
+	{
+		thumb: 'images/07-thumb.jpg',
+		alt: 'Plantation',
+		src: 'images/07.jpg',
+		caption: 'I drove past this plantation yesterday, everything is so green!',
+	},
+	{
+		thumb: 'images/08-thumb.jpg',
+		alt: 'Dunes',
+		src: 'images/08.jpg',
+		caption: 'My summer vacation to the Oregon Coast. I love the sandy dunes!',
+	},
+	{
+		thumb: 'images/09-thumb.jpg',
+		alt: 'Countryside Lane',
+		src: 'images/09.jpg',
+		caption: 'We enjoyed a quiet stroll down this countryside lane.',
+	},
+	{
+		thumb: 'images/10-thumb.jpg',
+		alt: 'Sunset',
+		src: 'images/10.jpg',
+		caption: 'Sunset at the coast! The sky turned a lovely shade of orange.',
+	},
+	{
+		thumb: 'images/11-thumb.jpg',
+		alt: 'Cave',
+		src: 'images/11.jpg',
+		caption: 'I did a tour of a cave today and the view of the landscape below was breathtaking.',
+	},
+	{
+		thumb: 'images/12-thumb.jpg',
+		alt: 'Bluebells',
+		src: 'images/12.jpg',
+		caption: 'I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in.',
+	},
 ];
 
 let photoCell = (cell) => {
+	for ( prop in cell ) {
 		return `
-		<a class="img_cell">
-			<img src="${cell[0]}" alt="${cell[1]}">
-			<div class="img_cell-overlay"><span>Click for large view</span></div>
-		</a>
-		<figure class="lg_img">
-			<img src="${cell[2]}" alt="${cell[1]}">
-			<figcaption>${cell[3]}</figcaption>
-		</figure>
-	`;
+			<a class="img_cell">
+				<img src="${cell['thumb']}" alt="${cell['alt']}">
+				<div class="img_cell-overlay"><span>Click for large view</span></div>
+			</a>
+			<figure class="lg_img">
+				<img src="${cell['src']}" alt="${cell['alt']}">
+				<figcaption>${cell['caption']}</figcaption>
+			</figure>
+		`;
+	}
 }
 
 function generateGallery(arr) {
